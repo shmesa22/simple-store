c2459b40214273676a45f19bc9e7713d9e51da09
Simon Hoyos <shoyos@helloelephant.com>
2018-03-12 12:09:35 -0500

    Convert json to js object

---
+++ src/productsList.js
@@ -0,0 +1,104 @@
+import product1 from './static/products/Product-1.jpg';
+import product2 from './static/products/Product-2.jpg';
+import product3 from './static/products/Product-3.jpg';
+import product4 from './static/products/Product-4.jpg';
+import product5 from './static/products/Product-5.jpg';
+import product6 from './static/products/Product-6.jpg';
+import product7 from './static/products/Product-7.jpg';
+import product8 from './static/products/Product-8.jpg';
+import product9 from './static/products/Product-9.jpg';
+
+export default {
+  shoes: [
+    {
+      name: 'Yeah!',
+      price: 125,
+      availability: 32,
+      colour: 'magenta',
+      style: 'street',
+      SKU: 3722912746,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product1,
+    },
+    {
+      name: 'My Boo',
+      price: 247,
+      availability: 32,
+      colour: 'ice blue',
+      style: 'yoga',
+      SKU: 1958443364,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product2,
+    },
+    {
+      name: 'Confessions Part II',
+      price: 136,
+      availability: 32,
+      colour: 'lime green',
+      style: 'flying',
+      SKU: 1958443364,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product3,
+    },
+    {
+      name: 'Love in This Club',
+      price: 258,
+      availability: 32,
+      colour: 'purple',
+      style: 'gym',
+      SKU: 7281408021,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product4,
+    },
+    {
+      name: 'U Remind Me',
+      price: 280,
+      availability: 32,
+      colour: 'black',
+      style: 'running',
+      SKU: 7668807667,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product5,
+    },
+    {
+      name: 'Yeah!',
+      price: 125,
+      availability: 32,
+      colour: 'magenta',
+      style: 'street',
+      SKU: 3722912746,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product6,
+    },
+    {
+      name: 'My Boo',
+      price: 247,
+      availability: 32,
+      colour: 'ice blue',
+      style: 'yoga',
+      SKU: 1958443364,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product7,
+    },
+    {
+      name: 'Confessions Part II',
+      price: 136,
+      availability: 32,
+      colour: 'lime green',
+      style: 'flying',
+      SKU: 1958443364,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product8,
+    },
+    {
+      name: 'Love in This Club',
+      price: 258,
+      availability: 32,
+      colour: 'purple',
+      style: 'gym',
+      SKU: 7281408021,
+      url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+      img: product9,
+    },
+  ],
+};
