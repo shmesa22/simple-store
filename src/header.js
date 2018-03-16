5a447174428920532b903baafab1d0bf70af697b
Simon Hoyos <shoyos@helloelephant.com>
2018-03-16 17:08:34 -0500

    Add store header and hamburguer logo

--- src/header.js
+++ src/header.js
@@ -1,8 +1,10 @@
 import logo from './static/Logo.png';
+import Hamburguer from './static/Hamburguer.svg';
 
 export const header = `
   <header class='header'>
     <img class='header__logo' src='${logo}' alt='store logo' />
+    <img class='header__hamburguer' src='${Hamburguer} alt='hamburguer menu'  />
     <div class='header__hero'><div>
   </header>
 `;
--- src/product.js
+++ src/product.js
@@ -9,11 +9,17 @@ function renderProduct(product) {
     <div class="card">
       <img class="card__image" src='${img}' alt='${name} ${colour}' />
       <h3 class="card__title">${name}</h3>
-      <p class="card__price>$${price}</p>
+      <p class="card__price">$${price}</p>
     </div>
   `;
 }
 
 export const renderProducts = `
-  ${shoes.map(renderProduct).join('')}
+  <section class="store">
+    <div class="store__header">
+      <h2 class="store__title">A super title or something else here</h2>
+      <p class="store__counter">${shoes.length} productos</p>
+    </div>
+    ${shoes.map(renderProduct).join('')}
+  </section>
 `;
