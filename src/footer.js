e935e86f57062476ed627985b68d3ace3dd468d6
Simon Hoyos <shoyos@helloelephant.com>
2018-03-25 13:51:43 -0500

    Add footer

---
+++ src/footer.js
@@ -0,0 +1,5 @@
+export const footer = `
+  <footer class="footer">
+    <span class="footer__rights">Something here all rights reserver - 2018</span>
+  </footer>
+`;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
+import { footer } from './footer';
 import { header } from './header';
 import { renderProducts } from './product';
 
 const root = document.querySelector('.root');
 
-root.innerHTML = header + renderProducts;
+root.innerHTML = header + renderProducts + footer;
